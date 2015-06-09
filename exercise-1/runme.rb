931e5edbb044c93e296bc75983532d060badfe3b
Josh Nesbitt <josh@wearestac.com>
2015-06-09 11:44:36 +0100

    Fix typo in exercise-1.

--- exercise-1/runme.rb
+++ exercise-1/runme.rb
@@ -2,6 +2,4 @@ start_time = Time.now
 puts "Congratulations you've completed  the first exercise at: #{start_time}"
 
 random_number = rand(99999)
-puts "You're random number is: #{random_number}"
-
-
+puts "Your random number is: #{random_number}"
