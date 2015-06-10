346a341491cc6fb7ee5de514669439cd8ea8a7c9
leon <leon_a_d@yahoo.com>
2015-06-10 13:54:26 +0100

    are for demos added

---
+++ demos/logic.rb
@@ -0,0 +1,24 @@
+#logical statements for assignment
+#terneary operators
+#Lazy assignment operator
+
+#Control Structures
+
+#If statement
+
+#logical operators
+
+##if !!nil
+
+#for loop
+
+#Unless statement
+
+#case statement
+
+#While loop
+
+#Until loop
+# Give example
+
+#statements on one line
