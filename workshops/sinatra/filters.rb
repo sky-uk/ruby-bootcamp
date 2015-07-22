ab03a11da9fcdeff29ac48a1a76ab4a0f342ccdc
leon <leon_a_d@yahoo.com>
2015-07-22 10:44:52 +0100

    moving exercises

---
+++ workshops/sinatra/filters.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'json'
+
+before do
+  puts "prints every time"
+end
+
+before %r[/greetings/\d{1}] do
+  puts "only printing when I am supposed to"
+end
+
+after do
+  puts "finishing up"
+end
+
+
+helpers do
+  def greetings
+    {1 => 'Good Day', 2 => 'hello' }
+  end
+end
+
+get '/greetings' do
+  content_type 'application/json'
+  greetings.to_json
+end
+
+get '/greetings/:id' do
+  greetings[params[:id].to_i]
+end
+
---
+++ workshops/sinatra/helpers.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+helpers do
+  def greetings id
+    {1 => 'hello', 2 => 'good day'}[id]
+  end
+end
+
+get '/greetings/:id' do
+  greetings(params[:id].to_i)
+end
+
