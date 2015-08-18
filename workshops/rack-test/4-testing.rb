9420b345a2c4a3ed09aee29752d9512ca5fa28b0
Josh Nesbitt <josh@wearestac.com>
2015-08-18 15:32:21 +0100

    Moved Rack::Test example over to rspec.

--- workshops/rack-test/4-testing.rb
+++ workshops/rack-test/4-testing.rb
@@ -44,28 +44,10 @@ end
 
 ENV['RACK_ENV'] = 'test'
 
-require 'test/unit'
+require 'rspec'
 require 'rack/test'
 
-class HelloWorldTest < Test::Unit::TestCase
-  include Rack::Test::Methods
-
-  def test_it_authenticates_with_correct_credentials
-    post('/authenticate', username: 'josh', password: 'password')
-
-    assert last_response.ok?
-    assert body['success']
-    assert body['token'].size == 32
-  end
-
-  def test_it_fails_to_authenticate_with_bad_credentials
-    post('/authenticate', username: 'josh', password: 'incorrect')
-
-    assert !last_response.ok?
-    assert !body['success']
-    assert body['token'].nil?
-  end
-
+module Helpers
 private
 
   def app
@@ -75,4 +57,48 @@ private
   def body
     JSON.parse(last_response.body)
   end
+end
+
+RSpec.configure do |c|
+  c.include Rack::Test::Methods
+  c.include Helpers
+end
+
+describe 'examples of Rack::Test being used in RSpec' do
+
+  let(:username) {}
+  let(:password) {}
+
+  subject! { post('/authenticate', username: username, password: password) }
+
+  context 'correct credentials' do
+
+    let(:username) { 'josh' }
+    let(:password) { 'password' }
+
+    it 'should succeed with the correct authentication credentials' do
+      post('/authenticate', username: 'josh', password: 'password')
+
+      expect(last_response.ok?).to be_truthy
+      expect(body['success']).to be_truthy
+      expect(body['token'].size).to be(32)
+    end
+
+  end
+
+  context 'incorrect credentials' do
+
+    let(:username) { 'josh' }
+    let(:password) { 'incorrect' }
+
+    it 'should fail with incorrect credentials' do
+
+      expect(last_response.ok?).to be_falsy
+      expect(body['success']).to be_falsy
+      expect(body['token']).to be_nil
+
+    end
+
+  end
+
 end
