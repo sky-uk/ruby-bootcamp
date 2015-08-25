b0eef183c6ff88b7b94528687a1eaeecfc772f2a
Josh Nesbitt <josh@wearestac.com>
2015-08-25 16:20:12 +0100

    Cleaned up collections exercise.

--- exercises/collections/clever_collections.rb
+++ exercises/collections/clever_collections.rb
@@ -1,12 +1,11 @@
 prices = {
-      'orange' => 10,
-      'apple' => 20,
-      'bread' => 100,
-      'tomato' => 25,
-      'cereal' => 234
+  'orange' => 10,
+  'apple' => 20,
+  'bread' => 100,
+  'tomato' => 25,
+  'cereal' => 234
 }
 
-shopping_list = [:orange, :apple, :apple, :cereal, :bread]
+shopping_list = [ :orange, :apple, :apple, :cereal, :bread ]
 
-
-#TODO - print out in pounds and pence the total for the shopping list
+# TODO: Print out in pounds and pence the total for the shopping list
